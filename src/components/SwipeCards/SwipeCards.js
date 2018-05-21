6a2abbc6bafb320d070811477f21b8bd9675bbb4
jraleman <jraleman@bendiburg.com>
2018-05-20 23:59:09 -0700

    Add StarRating example

--- src/components/SwipeCards/SwipeCards.js
+++ src/components/SwipeCards/SwipeCards.js
@@ -4,16 +4,32 @@ import {
   Text
 } from 'react-native';
 
+import StarRating from 'react-native-star-rating';
+
 export default class SwipeCards extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      starCount: 3.5
+    };
     return;
+  }
+  onStarRatingPress(rating) {
+    this.setState({
+      starCount: rating
+    });
   }
   render() {
     return (
       <View>
         <Text>{ "This is a card" }</Text>
+        <Text>{ "This is a rating" }</Text>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          rating={this.state.starCount}
+          selectedStar={(rating) => this.onStarRatingPress(rating)}
+        />
       </View>
     );
   }
